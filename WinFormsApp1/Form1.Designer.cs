d3cf30c05fdb6491ba523143db1433422afd0e28
4oddy <91286391+4oddy@users.noreply.github.com>
2021-10-09 15:19:28 +0300

    Add files via upload

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(54, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите параметры пароля";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Длина пароля";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(3, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(97, 80);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(145, 30);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Только цифры";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.Location = new System.Drawing.Point(97, 107);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(137, 30);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Только буквы";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox3.Location = new System.Drawing.Point(97, 134);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(156, 30);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Буквы и цифры";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(184, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Скопировать";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 250);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(349, 27);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.UseWaitCursor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox4.Location = new System.Drawing.Point(97, 160);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(286, 30);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "Использовать верхний регистр";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 303);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Key Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            MaximizeBox = false;
+            InitializeComponent();
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int length = 0;
+            int con = 0;
+            try
+            {
+                con = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+
+            }
+            if (con <= 30)
+            {
+                try
+                {
+                    length = Convert.ToInt32(textBox1.Text);
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                
+            }
+            
+            string password = string.Empty;
+
+            if (checkBox1.Checked == true)
+            {
+                string symb = "1 2 3 4 5 6 7 8 9 0";
+                string[] symbols = symb.Split();
+                for (int i = 0; i < length; i++)
+                {
+                    Random inr = new Random();
+                    string symbol = symbols[inr.Next(0, symbols.Length - 1)];
+                    password += symbol;
+
+                }
+                if (password is not null)
+                {
+                    textBox2.Text = password;
+                }
+
+            }
+
+
+            else if (checkBox2.Checked == true && checkBox4.Checked == true)
+            {
+                string symb = "q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M";
+                string[] symbols = symb.Split();
+                for (int i = 0; i < length; i++)
+                {
+                    Random inr = new Random();
+                    string symbol = symbols[inr.Next(0, symbols.Length - 1)];
+                    password += symbol;
+                }
+                if (password is not null)
+                {
+                    textBox2.Text = password;
+                }
+            }
+            else if (checkBox3.Checked == true && checkBox4.Checked == true)
+            {
+                string symb = "q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M 1 2 3 4 5 6 7 8 9 0";
+                string[] symbols = symb.Split();
+                for (int i = 0; i < length; i++)
+                {
+                    Random inr = new Random();
+                    string symbol = symbols[inr.Next(0, symbols.Length - 1)];
+                    password += symbol;
+
+                }
+                if (password is not null)
+                {
+                    textBox2.Text = password;
+                }
+            }
+
+            else if (checkBox2.Checked == true && checkBox4.Checked == false)
+            {
+                string symb = "q w e r t y u i o p a s d f g h j k l z x c v b n m";
+                string[] symbols = symb.Split();
+                for (int i = 0; i < length; i++)
+                {
+                    Random inr = new Random();
+                    string symbol = symbols[inr.Next(0, symbols.Length - 1)];
+                    password += symbol;
+
+                }
+                if (password is not null)
+                {
+                    textBox2.Text = password;
+                }
+            }
+
+            else if (checkBox3.Checked == true && checkBox4.Checked == false)
+            {
+                string symb = "q w e r t y u i o p a s d f g h j k l z x c v b n m 1 2 3 4 5 6 7 8 9 0";
+                string [] symbols = symb.Split();
+                for (int i = 0; i < length; i++)
+                {
+                    Random inr = new Random();
+                    string symbol = symbols[inr.Next(0, symbols.Length - 1)];
+                    password += symbol;
+                }
+                if (password is not null)
+                {
+                    textBox2.Text = password;
+                }
+            }
+
+            else
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string password = textBox2.Text;
+            if (password != null)
+            {
+                try
+                {
+                    Clipboard.SetText(password);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox4.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                checkBox2.Checked = false;
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {   
+            if (checkBox4.Checked == true)
+            {
+                checkBox1.Checked = false;
+            }
+           
+        }
+    }
+}
